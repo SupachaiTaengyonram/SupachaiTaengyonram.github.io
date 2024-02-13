0e5a0b2c1f4d467e5667c4ffed87ee52fd9e2fb4
supachai taengyonram <supachai.ta.66@ubu.ac.th>
2024-02-13 16:36:52 +0700

    force push

---
+++ app01.py
@@ -0,0 +1,12 @@
+import streamlit as st
+st.header('My OOP Notebook')
+#streamlit run streamlit/app01.py
+st.title('OOP in ML')
+st.sidebar.text('SideBar')
+st.sidebar.button('go')
+st.sidebar.button('went')
+st.sidebar.button('gone')
+
+st.title('OOP in ML')
+st.image("https://github.com/SupachaiTaengyonram/SupachaiTaengyonram.github.io/assets/159877866/df895340-f45e-4ffc-ad76-380c5fb77cf5")
+st.text_input("What's your name?")
